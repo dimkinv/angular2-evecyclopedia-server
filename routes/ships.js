6e8284823fc07ec1bc8d021b50f6fbb85253aed2
Danny Vernovsky <dv696w@att.com>
2016-11-13 14:05:31 +0200

    added POST for ships

--- routes/ships.js
+++ routes/ships.js
@@ -3,8 +3,8 @@ var express = require('express');
 var _ = require('lodash');
 var data_storage_1 = require('../data-import/data-storage');
 exports.router = express.Router({ mergeParams: true });
+var result = JSON.parse(data_storage_1.data);
 exports.router.get('/', function (req, res) {
-    var result = JSON.parse(data_storage_1.data);
     var race = findRace(result, req);
     var response = _.map(race.ships, function (ship) {
         return {
@@ -16,9 +16,15 @@ exports.router.get('/', function (req, res) {
     res.json(response);
 });
 exports.router.get('/:shipId', function (req, res) {
-    var result = JSON.parse(data_storage_1.data);
     var race = findRace(result, req);
     res.send(_.find(race.ships, { name: req.params.shipId }));
+});
+exports.router.post('/', function (req, res) {
+    var race = findRace(result, req);
+    req.body.race = req.params.raceId;
+    req.body.group = req.params.groupId;
+    race.ships.push(req.body);
+    res.send(201);
 });
 function findRace(result, req) {
     var races = _.find(result, function (group) {
--- routes/ships.ts
+++ routes/ships.ts
@@ -4,9 +4,9 @@ import {data} from '../data-import/data-storage'
 import {IGroup} from '../data-import/data-import'
 
 export let router = express.Router({mergeParams: true});
+var result: Array<IGroup> = JSON.parse(data);
 
 router.get('/', (req: express.Request, res: express.Response)=> {
-    var result: Array<IGroup> = JSON.parse(data);
     let race = findRace(result, req);
 
     let response = _.map(race.ships, (ship)=> {
@@ -21,10 +21,18 @@ router.get('/', (req: express.Request, res: express.Response)=> {
 });
 
 router.get('/:shipId', (req: express.Request, res: express.Response)=> {
-    var result: Array<IGroup> = JSON.parse(data);
     let race = findRace(result, req);
 
-    res.send( _.find(race.ships, {name: req.params.shipId}));
+    res.send(_.find(race.ships, {name: req.params.shipId}));
+});
+
+router.post('/', (req: express.Request, res: express.Response)=> {
+    let race = findRace(result, req);
+    req.body.race = req.params.raceId;
+    req.body.group = req.params.groupId;
+
+    race.ships.push(req.body);
+    res.send(201);
 });
 
 function findRace(result, req) {
