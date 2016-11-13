db8107876d99613b641a3039cea557a0e4e8d3fa
Danny Vernovsky <dv696w@att.com>
2016-11-13 12:36:36 +0200

    renamed icons folder, added icons to static path

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'attribute-icons')));
 
 app.use('/api', routes.router);
 
