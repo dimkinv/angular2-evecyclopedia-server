79b2f64b6eee93ad62387810d2047c1490ff55fd
Danny Vernovsky <dv696w@att.com>
2016-12-21 15:57:51 +0200

    added error enpoint

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,13 @@ var ships = require('./ships');
 exports.router = express.Router();
 exports.router.use('/groups', groups.router);
 exports.router.use('/ships', ships.router);
+var counter = 0;
+exports.router.get('/error', function (req, res) {
+    counter++;
+    if (counter < 3) {
+        res.status(500).end('error');
+        return;
+    }
+    counter = 0;
+    res.send('ok');
+});
--- routes/index.ts
+++ routes/index.ts
@@ -5,3 +5,15 @@ import * as ships from './ships';
 export var router = express.Router();
 router.use('/groups', groups.router);
 router.use('/ships', ships.router);
+
+let counter = 0;
+router.get('/error', (req, res)=>{
+    counter++;
+    if(counter< 3){
+        res.status(500).end('error');
+        return;
+    }
+
+    counter = 0;
+    res.send('ok');
+});
